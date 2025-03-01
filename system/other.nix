3e7020bb54b52a03d637217bb238ad5df3d5bb8b
Sophia Morningstar <sophia@shiningtrapezohedron.com>
2025-03-01 17:57:01 +0000

    Upgrade to Plasma 6

--- system/other.nix
+++ system/other.nix
@@ -19,7 +19,7 @@
   # Desktop
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   users.users.sophia = {
     shell = pkgs.zsh;

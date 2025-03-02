21658dd8cfbfb4f47b84f4f2db922a6018a81bbd
Sophia Morningstar <sophia@shiningtrapezohedron.com>
2025-03-02 19:57:51 +0000

    Dconf

--- config/dconf.nix
+++ config/dconf.nix
@@ -1,44 +1,51 @@
-{lib, ...}:
-with lib.hm.gvariant; {
-  dconf.settings = {
-    "org/gnome/settings-daemon/plugins/power" = {
-      sleep-inactive-ac-type = "logout";
-    };
+{ pkgs, lib, ... }: {
+  home.packages = with pkgs; [
+    dconf-editor
+  ];
 
-    "apps/seahorse/listing" = {
-      keyrings-selected = ["openssh:///home/sophia/.ssh"];
-    };
+  dconf = with lib.hm.gvariant; {
+    enable = true;
+    settings = {
+      "org/gnome/settings-daemon/plugins/power" = {
+        sleep-inactive-ac-type = "logout";
+      };
 
-    "org/gnome/desktop/a11y/applications" = {
-      screen-reader-enabled = false;
-    };
+      "apps/seahorse/listing" = {
+        keyrings-selected = ["openssh:///home/sophia/.ssh"];
+      };
 
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      cursor-size = 24;
-      cursor-theme = "Adwaita";
-      enable-animations = true;
-      font-name = "Monoid Nerd Font Mono,  10";
-      icon-theme = "breeze-dark";
-      scaling-factor = mkUint32 1;
-      text-scaling-factor = 1.0;
-      toolbar-style = "text";
-    };
+      "org/gnome/desktop/a11y/applications" = {
+        screen-reader-enabled = false;
+      };
 
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = "icon:minimize,maximize,close";
-    };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        cursor-size = 24;
+        cursor-theme = "Adwaita";
+        enable-animations = true;
+        font-name = "Monoid Nerd Font Mono,  10";
+        icon-theme = "breeze-dark";
+        scaling-factor = mkUint32 1;
+        text-scaling-factor = 1.0;
+        toolbar-style = "text";
+        clock-show-weekday = true;
+      };
 
-    "org/gtk/settings/file-chooser" = {
-      date-format = "regular";
-      location-mode = "path-bar";
-      show-hidden = false;
-      show-size-column = true;
-      show-type-column = true;
-      sort-column = "name";
-      sort-directories-first = false;
-      sort-order = "ascending";
-      type-format = "category";
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "icon:minimize,maximize,close";
+      };
+
+      "org/gtk/settings/file-chooser" = {
+        date-format = "regular";
+        location-mode = "path-bar";
+        show-hidden = false;
+        show-size-column = true;
+        show-type-column = true;
+        sort-column = "name";
+        sort-directories-first = false;
+        sort-order = "ascending";
+        type-format = "category";
+      };
     };
   };
 }

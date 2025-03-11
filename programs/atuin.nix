b61ce9d723a079d683c9fb5267a3682bd6734bd4
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-03-11 19:38:55 +0000

    Don't invert Atuin UI

--- programs/atuin.nix
+++ programs/atuin.nix
@@ -6,7 +6,6 @@
       update_check = false;
       filter_mode = "global";
       filter_mode_shell_up_key_binding = "workspace";
-      invert = true;
       exit_mode = "return-query";
       store_failed = true;
       enter_accept = true;

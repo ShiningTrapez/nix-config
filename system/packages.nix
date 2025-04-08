df8c0df8c2b875f637ff64ac949b428baad93403
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-04-08 11:44:44 +0100

    Add FFMPEG Full

--- system/packages.nix
+++ system/packages.nix
@@ -2,6 +2,7 @@
   hardware.opentabletdriver.enable = true;
 
   environment.systemPackages = with pkgs; [
+    ((ffmpeg-full.override { withUnfree = true; }).overrideAttrs (_: { doCheck = false; }))
     glibcLocales # https://github.com/NixOS/nixpkgs/issues/8398#issuecomment-186832814
     appimage-run
     corefonts

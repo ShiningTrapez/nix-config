402db3946033594b5068cb2e5e07957778338163
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-08-16 02:04:54 +0100

    Formatting

--- overlays.nix
+++ overlays.nix
@@ -1,14 +1,14 @@
 {pkgs, ...}: {
   nixpkgs.overlays = [
     (_: prev: {
-      sddm-sugar-dark = (pkgs.libsForQt5.callPackage ./packages/sddm-sugar-dark.nix {
+      sddm-sugar-dark = pkgs.libsForQt5.callPackage ./packages/sddm-sugar-dark.nix {
         configOverrides = pkgs.writeText "custom-theme.conf" ''
           [General]
           # Force default
           ScreenWidth=
           ScreenHeight=
         '';
-      });
+      };
 
       vicinae = pkgs.callPackage ./packages/vicinae.nix {};
     })

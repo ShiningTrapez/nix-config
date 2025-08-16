04ab60fc61988953b409e72a632f9a26607bcebd
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-08-16 05:45:56 +0100

    Config option for CPU Mitigations

--- system/boot.nix
+++ system/boot.nix
@@ -1,35 +1,46 @@
-{pkgs, ...}: {
-  boot = {
+{pkgs, lib, config, ...}: let
+  inherit (lib) mkOption types optionals;
+
+  mitigationParams = [
+    "noibrs"
+    "noibpb"
+    "nopti"
+    "nospectre_v2"
+    "nospectre_v1"
+    "l1tf=off"
+    "nospec_store_bypass_disable"
+    "no_stf_barrier"
+    "mds=off"
+    "tsx=on"
+    "tsx_async_abort=off"
+    "mitigations=off"
+  ];
+
+  baseParams = [
+    "sysrq_always_enabled=1"
+    "quiet"
+    "splash"
+    "loglevel=3"
+    "boot.shell_on_fail"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+    "rd.udev.log_level=3"
+    "kernel.nmi_watchdog=0"
+    "nowatchdog"
+  ];
+in {
+  options.performance.insecureDisableMitigations = mkOption {
+    type = types.bool;
+    default = false;
+    description = "Disable CPU Security mitigations for Performance (insecure).";
+  };
+
+  config.boot = {
     tmp.cleanOnBoot = true;
-
     initrd.verbose = false;
-
     consoleLogLevel = 3;
 
-    kernelParams = [
-      "noibrs"
-      "noibpb"
-      "nopti"
-      "nospectre_v2"
-      "nospectre_v1"
-      "l1tf=off"
-      "nospec_store_bypass_disable"
-      "no_stf_barrier"
-      "mds=off"
-      "tsx=on"
-      "tsx_async_abort=off"
-      "mitigations=off"
-      "sysrq_always_enabled=1"
-      "quiet"
-      "splash"
-      "loglevel=3"
-      "boot.shell_on_fail"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-      "rd.udev.log_level=3"
-      "kernel.nmi_watchdog=0"
-      "nowatchdog"
-    ];
+    kernelParams = baseParams ++ optionals config.performance.insecureDisableMitigations mitigationParams;
 
     loader = {
       systemd-boot.enable = true;

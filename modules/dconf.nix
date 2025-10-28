2c77e9b363d19828530ed1a0418ec961d270b89f
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-10-28 12:55:24 +0000

    Remove Vicinae

--- modules/dconf.nix
+++ modules/dconf.nix
@@ -63,12 +63,6 @@
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         ];
       };
-
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "Vicinae";
-        command = "${pkgs.vicinae}/bin/vicinae";
-        binding = "<Super>space";
-      };
     };
   };
 }

ab0eb06ee61b8a91c655bfb1be97204d5ea880eb
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-11-24 12:15:09 +0000

    Switch to non open nVidia

--- system/graphics.nix
+++ system/graphics.nix
@@ -3,7 +3,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     modesetting.enable = true;
     powerManagement.enable = false;
-    open = true;
+    open = false;
     nvidiaSettings = true;
   };
 

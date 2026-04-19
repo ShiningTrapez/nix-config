67d7b8beb1ed6c2fbfa4d03a7cf839d09f3c98f0
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2026-04-19 17:05:59 +0100

    Fix Kitty Font

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,6 +1,10 @@
 _: {
   programs.kitty = {
     enable = true;
+     font = {
+      name = "Iosevka Nerd Font Mono";
+      size = 14;
+    };
     settings = {
       confirm_os_window_close = 0;
     };
